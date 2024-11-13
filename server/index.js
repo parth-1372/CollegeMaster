7f6604d6528e037f8a7242bddf919ba9e861c32b
Parth <parthmungra1372@gmail.com>
2024-11-13 16:14:27 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,53 +1,4 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// import adminRoutes from "./routes/adminRoutes.js";
-// import studentRoutes from "./routes/studentRoutes.js";
-// import facultyRoutes from "./routes/facultyRoutes.js";
-// import { addDummyAdmin } from "./controller/adminController.js";
-// const app = express();
-// dotenv.config();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
-
-// // app.use(cors({
-// //     origin: 'https://universitymanagementsystem.vercel.app', // Allow Vercel frontend
-// //     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
-// //     credentials: true // Allow cookies to be sent if needed
-// // }));
-// const cors = require("cors");
-// app.use(cors({
-//     origin: function(origin, callback){
-//       return callback(null, true);
-//     },
-//     credentials: true
-// }));
-
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/faculty", facultyRoutes);
-// app.use("/api/student", studentRoutes);
-
-// const PORT = process.env.PORT || 5001;
-// app.get("/", (req, res) => {
-//   res.send("Hello to college erp API");
-// });
-// mongoose
-//   .connect("mongodb+srv://Parth:0WwdWHT4YCTDOZh5@cluster0.f8axp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     addDummyAdmin();
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((error) => console.log("Mongo Error", error.message));
-
-
- 
-  import express from "express";
+ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"; // Changed to ES module import
@@ -65,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up CORS
 app.use(cors({
-  origin: 'https://universitymanagementsystem.vercel.app',
+  origin: "*" ,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
